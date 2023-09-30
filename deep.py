71f6e0b2d03b4a8a5a77f4612e0580d0e33645b4
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2023-09-30 20:48:11 +0200

    deep part is added #12

---
+++ deep.py
@@ -0,0 +1,72 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense, Dropout
+from keras.utils import to_categorical
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Load the dataset (replace 'your_dataset.csv' with your actual dataset file)
+data = pd.read_csv('your_dataset.csv')
+
+# Split the data into features (X) and labels (y)
+X = data['Text']
+y = data['Sentiment']
+
+# Encode the labels
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Tokenize the text data
+max_words = 1000  # Maximum number of words to keep based on word frequency
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(X_train)
+
+# Convert text to sequences
+X_train_seq = tokenizer.texts_to_sequences(X_train)
+X_test_seq = tokenizer.texts_to_sequences(X_test)
+
+# Pad sequences to ensure consistent input length
+max_sequence_length = 100  # You can adjust this value as needed
+X_train_pad = pad_sequences(X_train_seq, maxlen=max_sequence_length)
+X_test_pad = pad_sequences(X_test_seq, maxlen=max_sequence_length)
+
+# Create a simple feedforward neural network model
+model = Sequential()
+model.add(Embedding(input_dim=max_words, output_dim=128, input_length=max_sequence_length))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3, activation='softmax'))  # 3 output classes (positive, negative, neutral)
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Convert labels to one-hot encoding
+y_train_onehot = to_categorical(y_train, num_classes=3)
+y_test_onehot = to_categorical(y_test, num_classes=3)
+
+# Train the model
+epochs = 10  # You can adjust this value
+batch_size = 32  # You can adjust this value
+model.fit(X_train_pad, y_train_onehot, epochs=epochs, batch_size=batch_size, validation_split=0.2)
+
+# Evaluate the model
+y_pred_onehot = model.predict(X_test_pad)
+y_pred = np.argmax(y_pred_onehot, axis=1)
+y_test = np.argmax(y_test_onehot, axis=1)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
+print('Classification Report:\n', report)
+print('Confusion Matrix:\n', confusion)
