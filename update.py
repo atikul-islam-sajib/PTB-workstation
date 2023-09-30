23d3917c1c369a1815ea843bbe56ed04f050d15b
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2023-09-30 20:01:25 +0200

    evaluation metrices is added #6

---
+++ update.py
@@ -0,0 +1,47 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+
+# Load the dataset (replace 'your_dataset.csv' with your actual dataset file)
+data = pd.read_csv('your_dataset.csv')
+
+# Split the data into features (X) and labels (y)
+X = data['Text']
+y = data['Sentiment']
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the TF-IDF vectorizer
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)  # You can adjust the number of features as needed
+
+# Transform the text data into TF-IDF features
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+# Initialize the Multinomial Naive Bayes classifier
+classifier = MultinomialNB()
+
+# Train the classifier on the training data
+classifier.fit(X_train_tfidf, y_train)
+
+# Make predictions on the test data
+y_pred = classifier.predict(X_test_tfidf)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
+print(f'Precision: {precision}')
+print(f'Recall: {recall}')
+print(f'F1 Score: {f1}')
+print('Confusion Matrix:\n', confusion)
+print('Classification Report:\n', report)
